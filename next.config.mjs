a411d420537b342657a06c90a802c76f2e681714
yuminji <yuminji97@kakao.com>
2026-01-22 15:37:40 +0900

    api 경로 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // IS_CAPACITOR 환경 변수가 있을 때만 export 모드 사용
+    output: process.env.IS_CAPACITOR ? 'export' : undefined,
     typescript: {
         ignoreBuildErrors: true,
     },
